ecd4c4c5fb91df5e167bce370b38b6ed53ee184c
Marc Schlagenhauf <marc@schlagenhauf.net>
2025-09-28 04:01:36 +0200

    Remove livewire-toaster

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,3 @@
-import '../../vendor/masmerise/livewire-toaster/resources/js';
-
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import sort from '@alpinejs/sort'
 Alpine.plugin(sort)
