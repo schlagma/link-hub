bd592f8d44ed7e0e359d93c9ee577b4e07f00fe6
Marc Schlagenhauf <marc@schlagenhauf.net>
2025-09-28 17:12:12 +0200

    Padding on public dashboard

--- resources/views/livewire/public/dashboard.blade.php
+++ resources/views/livewire/public/dashboard.blade.php
@@ -1,4 +1,4 @@
-<div class="p-6 sm:px-8 overflow-y-auto h-full">
+<div class="p-6 pt-8 sm:px-8 sm:pb-8 overflow-y-auto h-full">
     <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         @foreach($pages as $page)
             <flux:button
